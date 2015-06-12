406fcd5259f1f1c68c09befd3d1b98cb851ea422
Jafffy <jainersoer@gmail.com>
2015-06-12 10:27:48 +0900

    bug fix

--- Statistic.h
+++ Statistic.h
@@ -1,6 +1,9 @@
 #ifndef STATISTIC_H_
 #define STATISTIC_H_
 
+#include <deque>
+#include <set>
+
 class FPSStatistic
 {
 public:
@@ -9,20 +12,36 @@ public:
 
 	int numberOfFPS;
 
+	std::set<double> FPSs;
+
 	FPSStatistic() : meanFPS(0.0), stddevFPS(0.0), numberOfFPS(0) {
 
 	}
 
 	void update(int newFPS) {
-		meanFPS += newFPS;
-		stddevFPS += newFPS*newFPS;
-		++numberOfFPS;
+		FPSs.insert(newFPS);
 	}
 
 	void commit() {
-		meanFPS /= numberOfFPS;
-		stddevFPS /= numberOfFPS;
-		stddevFPS -= meanFPS;
+		std::deque<double> removal;
+		for (double fps : FPSs) {
+			removal.push_back(fps);
+		}
+
+		for (unsigned int i = 0; i < FPSs.size() * 0.15; ++i) {
+			removal.pop_front();
+			removal.pop_back();
+		}
+
+		for (double fps : removal) {
+			printf("%lf\n", fps);
+			meanFPS += fps;
+			stddevFPS += fps*fps;
+		}
+
+		meanFPS /= removal.size();
+		stddevFPS /= removal.size();
+		stddevFPS -= meanFPS*meanFPS;
 		stddevFPS = sqrt(stddevFPS);
 	}
 };
